cc0f9c4dfbb18306aba8694dd73c02031a2f134c
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-03-15 10:32:26 -0500

    refactor(web): idiomatic trapping of school parameter

--- lib/medians_web/controllers/school_controller.ex
+++ lib/medians_web/controllers/school_controller.ex
@@ -1,12 +1,12 @@
 defmodule MediansWeb.SchoolController do
   use MediansWeb, :controller
 
-  def index(conn, params) do
-    if params["school"] do
-      show(conn, params)
-    else
-      render(conn, :index, layout: false)
-    end
+  def index(conn, params(%{"school" => _})) do
+    show(conn, params)
+  end
+
+  def index(conn, _params) do
+    render(conn, :index, layout: false)
   end
 
   def show(conn, params) do
